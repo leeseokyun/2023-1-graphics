d485ab0700959892acc7d553c0d2661940036d7a
leeseokyun <127462098+leeseokyun@users.noreply.github.com>
2023-03-15 21:43:15 +0900

    Update box_texture.js

--- box_texture.js
+++ box_texture.js
@@ -6,10 +6,10 @@ function setup() {
   createCanvas(400, 400,WEBGL);
 }
 function draw() {
-  background(220);
-  //rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  //rotateZ(frameCount * 0.01);
-  texture(img);
-  box(100);
+  background(220); //없어도 됨
+  rotateX(frameCount * 0.01); //x 좌표로 돌리기 
+  rotateY(frameCount * 0.01); //y 좌표로 돌리기
+  rotateZ(frameCount * 0.01); //z 좌표로 돌리기
+  texture(img); //img 변수 넣기
+  box(200); //박스 크기
 }
